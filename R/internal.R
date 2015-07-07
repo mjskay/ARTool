3512310f6581084211888010cbbe32c6a5e11022
Matthew Kay <matthew.kay@gmail.com>
2015-07-06 22:45:55 -0700

    major reworking of parse.art.formula for robustness and to support Error() terms towards #2

--- R/internal.R
+++ R/internal.R
@@ -8,85 +8,82 @@
 ### Raises exception if formula does not validate (e.g. not factorial).
 ### Returns list with:
 ###		fixed.only: formula with only fixed components
-###		fixed.terms: additive formula with only fixed terms and no response 
+###		fixed.terms: additive formula with only variables from fixed terms and no response 
 ###							(e.g. for use with ddply)
 ###    	fixed.term.labels:	character vector of term labels
-###     all.terms: formula with response, fixed main effects, grouping factors (not in (1|x) form), 
-###                and fixed interaction terms (in that order)                
+###     n.grouping.variables: number of grouping variables (e.g. (1|d))
+###     n.error.variables: number of error variables (e.g. Error(g))
 parse.art.formula = function(formula) {
     #extract terms from the formula
     f.terms = terms(formula)
-    f.variables = attr(f.terms, "variables")
-    f.term.labels = attr(f.terms, "term.labels")
-    f.factors = attr(f.terms, "factors")
-    f.order = attr(f.terms, "order")
-    f.response = attr(f.terms, "response")
     
-    #ensure we have 1 independent variable
+    #ensure we have an independent variable and an intercept
     if (attr(f.terms, "response") != 1) {
-        stop("Model must have exactly one independent variable (got ", f.response, ")")
+        stop("Model must have exactly one dependent variable (got ", attr(f.terms, "response"), ")")
     }
-    
-    #ensure we have an intercept
     if (attr(f.terms, "intercept") == 0) {
         stop("Model must have an intercept (got ", attr(f.terms, "intercept"), ")")
     }
     
-    #determine which terms on the right hand side are grouping terms
-    is.grouping.variable = laply(as.list(f.variables), function(term) as.list(term)[[1]] == quote(`|`))
+    #unique variables in the rhs of the formula as list of quoted variables 
+    #e.g. y ~ a*b*c + (1|d) + Error(g) -> list(quote(a), quote(b), quote(c), quote((1|d)), quote(Error(g))))
+    variables = as.list(attr(f.terms, "variables"))[c(-1,-2)] 
+    #char vector of names of rhs terms and their interactions
+    #e.g. y ~ a*b*c + (1|d) + Error(g) -> c("a","b","c","1 | d","Error(g)","a:b","b:c","a:b:c"))  
+    term.labels = attr(f.terms, "term.labels")
+    #vector of length(f.term.labels); value is the order of the interaction of the corresponding entry in term.labels
+    #e.g. y ~ a*b*c + (1|d) + Error(g) -> c(1,1,1,1,1,2,2,3)  
+    term.order = attr(f.terms, "order")
+    
+    #determine which variables on the rhs are grouping variables, error variables, or fixed variables
+    is.grouping.variable = laply(variables, function(term) as.list(term)[[1]] == quote(`|`))
+    is.error.variable = laply(variables, function(term) is.call(term) & as.list(term)[[1]] == quote(`Error`))
+    #all other variables that aren't grouping or error variables must be fixed variables
+    is.fixed.variable = !(is.grouping.variable | is.error.variable)
+
+    #ensure we have at least one fixed effect and are using either grouping terms or error terms but not both
+    if (sum(is.fixed.variable) == 0) {
+        stop("Model must have at least one fixed effect (0 given)")
+    }
+    if (any(is.grouping.variable) & any(is.error.variable)) {
+        stop("Model cannot contain both grouping terms, like (1|d), and error terms, like Error(d). Use one or the other.")
+    }
+    
+    #get table with rows == rhs variables and cols == term labels, each cell == 1 if variable in term
+    variables.by.terms = attr(f.terms, "factors")[-1,,drop=FALSE] #prevent reducing to vector if only one cell
     
     #make a version of the formula terms with only fixed effects
-    fixed.f.variables = f.variables[!is.grouping.variable]
-    term_index.2 = length(f.term.labels) - length(is.grouping.variable) + 2	#TODO: go back and determine what this is 
-    if (term_index.2 < 0) {
-        #only happens when not factorial? TODO: why?
+    n.rhs.variables = length(variables)
+    n.rhs.terms = length(term.labels)
+    n.interaction.terms = n.rhs.terms - n.rhs.variables
+    if (n.interaction.terms < 0) {
+        #only happens when not factorial
         stop("Model must include all combinations of interactions of fixed effects.")
     }
-    term_index = c(!is.grouping.variable[-c(1,2)], rep(TRUE, term_index.2))
-    if (length(f.factors) == 0) {
-        #no effects => no fixed effects
-        stop("Model must have at least one fixed effect (0 given)")
-    }
-    fixed.f.factors = f.factors[!is.grouping.variable[-1],term_index]
-    if (length(fixed.f.factors) == 0) {
-        #all effects are random => no fixed effects
-        stop("Model must have at least one fixed effect (0 given)")
-    }
-    fixed.f.term.labels = f.term.labels[term_index]
-    fixed.f.order = f.order[term_index]
+    is.fixed.term = c(is.fixed.variable, rep(TRUE, n.interaction.terms))
+    fixed.variables.by.terms = variables.by.terms[is.fixed.variable, is.fixed.term, drop=FALSE]
+    fixed.term.labels = term.labels[is.fixed.term]
+    fixed.term.order = term.order[is.fixed.term]
     
     #ensure design of fixed effects portion of model has all interactions
     #first, pull out the response and the main (i.e. order-1) fixed effect terms 
-    response.label = rownames(f.factors)[1]
-    main.effects.labels = fixed.f.term.labels[fixed.f.order == 1]
+    response = formula[[2]]
+    main.effects.labels = fixed.term.labels[fixed.term.order == 1]
     #build a factorial model of all fixed effects
-    factorial.formula = eval(parse(text=paste(response.label,"~",do.call(paste, c(main.effects.labels, list(sep="*"))))))
+    factorial.formula = eval(bquote(.(response) ~ .(Reduce(function(x,y) bquote(.(x) * .(y)), Map(as.name, main.effects.labels)))))
     #verify the factorial model is the same as the fixed effects in the supplied model
-    factorial.factors = attr(terms(factorial.formula), "factors")
-    if (!all(dim(factorial.factors) == dim(fixed.f.factors)) || !all(factorial.factors == fixed.f.factors)) {
+    factorial.factors = attr(terms(factorial.formula), "factors")[-1,,drop=FALSE]
+    if (!all(dim(factorial.factors) == dim(fixed.variables.by.terms)) || !all(factorial.factors == fixed.variables.by.terms)) {
         stop("Model must include all combinations of interactions of fixed effects.")
-    }
-    
-    #	#ensure grouping terms are intercept-only (TODO: is this necessary?)
-    grouping.f.variables = f.variables[is.grouping.variable]
-    #	if (!all(laply(as.list(grouping.f.variables), function(term) term[[2]] == 1))) {
-    #		stop("All grouping variables must be intercept-only (e.g. (1|some.factor))")
-    #	}
-    
-    #generate all-terms formula
-    all.terms = factorial.formula
-    #add grouping variables back in to formula as standlone terms (not in (1|x) form)
-    for (rv in as.list(grouping.f.variables)) {
-        all.terms = eval(parse(text=paste("update(all.terms, ~ . +", as.character(rv[[3]]), ")")))
     }
     
     #return validated formulas
     list(
         fixed.only=factorial.formula,
-        fixed.terms=eval(parse(text=paste("~",do.call(paste, c(main.effects.labels, list(sep="+")))))),
-        fixed.term.labels=fixed.f.term.labels,
-        all.terms=all.terms,
-        n.grouping.variables=sum(is.grouping.variable)
+        fixed.terms=eval(bquote(~ .(Reduce(function(x,y) bquote(.(x) + .(y)), Map(as.name, main.effects.labels))))),
+        fixed.term.labels=fixed.term.labels,
+        n.grouping.variables=sum(is.grouping.variable),
+        n.error.variables=sum(is.error.variable)
     )
 }
 
--- tests/testthat/test.parse.art.formula.R
+++ tests/testthat/test.parse.art.formula.R
@@ -9,9 +9,10 @@ library(ARTool)
 context("art formula parsing")
 
 test_that("art formula must have 1 response variable", {
-    expect_error(ARTool:::parse.art.formula(~ a*b*c), 'Model must have exactly one independent variable \\(got 0\\)')
-    expect_error(ARTool:::parse.art.formula(~ 0),     'Model must have exactly one independent variable \\(got 0\\)')
-    expect_error(ARTool:::parse.art.formula(~ (1|c)), 'Model must have exactly one independent variable \\(got 0\\)')
+    expect_error(ARTool:::parse.art.formula(~ a*b*c), 'Model must have exactly one dependent variable \\(got 0\\)')
+    expect_error(ARTool:::parse.art.formula(~ 0),     'Model must have exactly one dependent variable \\(got 0\\)')
+    expect_error(ARTool:::parse.art.formula(~ (1|c)), 'Model must have exactly one dependent variable \\(got 0\\)')
+    expect_error(ARTool:::parse.art.formula(~ Error(c)), 'Model must have exactly one dependent variable \\(got 0\\)')
 })
 
 test_that("art formula must have an intercept", {
@@ -19,11 +20,13 @@ test_that("art formula must have an intercept", {
     expect_error(ARTool:::parse.art.formula(y ~ a*b*c - 1), 'Model must have an intercept \\(got 0\\)')
     expect_error(ARTool:::parse.art.formula(y ~ 0),         'Model must have an intercept \\(got 0\\)')
     expect_error(ARTool:::parse.art.formula(y ~ 0 + (1|c)), 'Model must have an intercept \\(got 0\\)')
+    expect_error(ARTool:::parse.art.formula(y ~ 0 + Error(c)), 'Model must have an intercept \\(got 0\\)')
 })
 
 test_that("art formula must have at least 1 fixed effect", {
     expect_error(ARTool:::parse.art.formula(y ~ 1),         'Model must have at least one fixed effect \\(0 given\\)')
     expect_error(ARTool:::parse.art.formula(y ~ 1 + (1|c)), 'Model must have at least one fixed effect \\(0 given\\)')
+    expect_error(ARTool:::parse.art.formula(y ~ 1 + Error(c)), 'Model must have at least one fixed effect \\(0 given\\)')
 })
 
 test_that("art formula must have all interactions of fixed effects", {
@@ -33,6 +36,7 @@ test_that("art formula must have all interactions of fixed effects", {
     expect_error(ARTool:::parse.art.formula(y ~ a * b + c:d),   'Model must include all combinations of interactions of fixed effects.')
     expect_error(ARTool:::parse.art.formula(y ~ a * b + c),     'Model must include all combinations of interactions of fixed effects.')
     expect_error(ARTool:::parse.art.formula(y ~ a + b + (1|c)), 'Model must include all combinations of interactions of fixed effects.')
+    expect_error(ARTool:::parse.art.formula(y ~ a + b + Error(c)), 'Model must include all combinations of interactions of fixed effects.')
 })
 
 test_that("different variations on factorial model specifications are accepted by art", {
@@ -40,12 +44,29 @@ test_that("different variations on factorial model specifications are accepted b
     expect_equal(ARTool:::parse.art.formula(y ~ a)$fixed.terms, ~ a)
     expect_equal(ARTool:::parse.art.formula(y ~ a + (1|c))$fixed.only, y ~ a)
     expect_equal(ARTool:::parse.art.formula(y ~ a + (1|c))$fixed.terms, ~ a)
+    expect_equal(ARTool:::parse.art.formula(y ~ a + Error(c))$fixed.only, y ~ a)
+    expect_equal(ARTool:::parse.art.formula(y ~ a + Error(c))$fixed.terms, ~ a)
     expect_equal(ARTool:::parse.art.formula(y ~ a * b)$fixed.only, y ~ a * b)
     expect_equal(ARTool:::parse.art.formula(y ~ a * b)$fixed.terms, ~ a + b)
     expect_equal(ARTool:::parse.art.formula(y ~ a * b + (1|c))$fixed.only, y ~ a * b)
     expect_equal(ARTool:::parse.art.formula(y ~ a * b + (1|c))$fixed.terms, ~ a + b)
+    expect_equal(ARTool:::parse.art.formula(y ~ a * b + Error(c))$fixed.only, y ~ a * b)
+    expect_equal(ARTool:::parse.art.formula(y ~ a * b + Error(c))$fixed.terms, ~ a + b)
     expect_equal(ARTool:::parse.art.formula(y ~ a:b + a + b)$fixed.only, y ~ a*b)
     expect_equal(ARTool:::parse.art.formula(y ~ a:b + a + b)$fixed.terms, ~ a + b)
     expect_equal(ARTool:::parse.art.formula(y ~ a:b + a + b + (1|c))$fixed.only, y ~ a*b)
     expect_equal(ARTool:::parse.art.formula(y ~ a:b + a + b + (1|c))$fixed.terms, ~ a + b)
+    expect_equal(ARTool:::parse.art.formula(y ~ a*b*c)$fixed.terms, ~ a + b + c)
+    expect_equal(ARTool:::parse.art.formula(y ~ a*b*c*d)$fixed.terms, ~ a + b + c + d)
+})
+
+test_that("grouping terms and error terms are counted correctly", {
+    expect_equal(ARTool:::parse.art.formula(y ~ a + (1|d))$n.grouping.variables, 1)
+    expect_equal(ARTool:::parse.art.formula(y ~ a + (1|d) + (1|g))$n.grouping.variables, 2)
+    expect_equal(ARTool:::parse.art.formula(y ~ a + Error(d))$n.error.variables, 1)
+    expect_equal(ARTool:::parse.art.formula(y ~ a + Error(d) + Error(g))$n.error.variables, 2)
+})
+
+test_that("grouping terms and error terms are counted correctly", {
+    expect_error(ARTool:::parse.art.formula(y ~ a + (1|d) + Error(g)), "Model cannot contain both grouping terms, like (1|d), and error terms, like Error(d). Use one or the other.", fixed=TRUE)
 })
