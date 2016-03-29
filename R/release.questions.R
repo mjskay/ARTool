a7c53caa1aef571e01ccfbf828e4bdea9a715d7b
Matthew Kay <matthew.kay@gmail.com>
2016-03-28 21:23:41 -0700

    add release questions for devtools::release

---
+++ R/release.questions.R
@@ -0,0 +1,11 @@
+# Release questions for devtools::release()
+# 
+# Author: mjskay
+###############################################################################
+
+
+release_questions <- function() {
+    c(
+        "Is README.md up to date?"
+    )
+}
